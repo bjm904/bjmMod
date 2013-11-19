429f835021727ea376bad751a6870d03360c41b2
Bryce Meyer <bjm904@gmail.com>
2013-11-18 23:02:02 -0800

    More Initial Commit... I think...

--- mcp/src/minecraft/bjm904/bjmMod.java
+++ mcp/src/minecraft/bjm904/bjmMod.java
@@ -1,8 +1,10 @@
 package bjm904;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod( modid = "bjmMod", name = "bjmMod", version = "0.0.1" )
+@NetworkMod( channels = {""}, clientSideRequired = true, serverSideRequired = true )
 
 public class bjmMod {
 
