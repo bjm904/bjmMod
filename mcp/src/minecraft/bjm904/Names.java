46fb7f839f62296ef2d041ab46819b4f44381695
Bryce Meyer <bjm904@gmail.com>
2013-11-19 15:54:08 -0800

    Change spelling of Mac

--- mcp/src/minecraft/bjm904/Names.java
+++ mcp/src/minecraft/bjm904/Names.java
@@ -2,37 +2,37 @@ package bjm904;
 
 public class Names {
 	public static final String macuahuitlDiamond_unlocalizedName = "macuahuitlDiamond";
-	public static final String macuahuitlDiamond_name = "Diamond Macuahuitl";
+	public static final String macuahuitlDiamond_name = "Diamond Maquahuitl";
 	public static final String macuahuitlGold_unlocalizedName = "macuahuitlGold";
-	public static final String macuahuitlGold_name = "Gold Macuahuitl";
+	public static final String macuahuitlGold_name = "Gold Maquahuitl";
 	public static final String macuahuitlIron_unlocalizedName = "macuahuitlIron";
-	public static final String macuahuitlIron_name = "Iron Macuahuitl";
+	public static final String macuahuitlIron_name = "Iron Maquahuitl";
 	public static final String macuahuitlStone_unlocalizedName = "macuahuitlStone";
-	public static final String macuahuitlStone_name = "Stone Macuahuitl";
+	public static final String macuahuitlStone_name = "Stone Maquahuitl";
 	public static final String macuahuitlWood_unlocalizedName = "macuahuitlWood";
-	public static final String macuahuitlWood_name = "Wood Macuahuitl";
+	public static final String macuahuitlWood_name = "Wood Maquahuitl";
 	
 	public static final String macuahuitlDiamondClub_unlocalizedName = "macuahuitlDiamondClub";
-	public static final String macuahuitlDiamondClub_name = "Diamond Macuahuitl Club";
+	public static final String macuahuitlDiamondClub_name = "Diamond Maquahuitl Club";
 	public static final String macuahuitlGoldClub_unlocalizedName = "macuahuitlGoldClub";
-	public static final String macuahuitlGoldClub_name = "Gold Macuahuitl Club";
+	public static final String macuahuitlGoldClub_name = "Gold Maquahuitl Club";
 	public static final String macuahuitlIronClub_unlocalizedName = "macuahuitlIronClub";
-	public static final String macuahuitlIronClub_name = "Iron Macuahuitl Club";
+	public static final String macuahuitlIronClub_name = "Iron Maquahuitl Club";
 	public static final String macuahuitlStoneClub_unlocalizedName = "macuahuitlStoneClub";
-	public static final String macuahuitlStoneClub_name = "Stone Macuahuitl Club";
+	public static final String macuahuitlStoneClub_name = "Stone Maquahuitl Club";
 	public static final String macuahuitlWoodClub_unlocalizedName = "macuahuitlWoodClub";
-	public static final String macuahuitlWoodClub_name = "Wood Macuahuitl Club";
+	public static final String macuahuitlWoodClub_name = "Wood Maquahuitl Club";
 	
 	public static final String macuahuitlDiamondStock_unlocalizedName = "macuahuitlDiamondStock";
-	public static final String macuahuitlDiamondStock_name = "Diamond Macuahuitl Stock";
+	public static final String macuahuitlDiamondStock_name = "Diamond Maquahuitl Stock";
 	public static final String macuahuitlGoldStock_unlocalizedName = "macuahuitlGoldStock";
-	public static final String macuahuitlGoldStock_name = "Gold Macuahuitl Stock";
+	public static final String macuahuitlGoldStock_name = "Gold Maquahuitl Stock";
 	public static final String macuahuitlIronStock_unlocalizedName = "macuahuitlIronStock";
-	public static final String macuahuitlIronStock_name = "Iron Macuahuitl Stock";
+	public static final String macuahuitlIronStock_name = "Iron Maquahuitl Stock";
 	public static final String macuahuitlStoneStock_unlocalizedName = "macuahuitlStoneStock";
-	public static final String macuahuitlStoneStock_name = "Stone Macuahuitl Stock";
+	public static final String macuahuitlStoneStock_name = "Stone Maquahuitl Stock";
 	public static final String macuahuitlWoodStock_unlocalizedName = "macuahuitlWoodStock";
-	public static final String macuahuitlWoodStock_name = "Wood Macuahuitl Stock";
+	public static final String macuahuitlWoodStock_name = "Wood Maquahuitl Stock";
 	
 	
 	public static final String nightendLog_unlocalizedName = "nightendLog";
